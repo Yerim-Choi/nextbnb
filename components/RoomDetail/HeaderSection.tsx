52209af6d42441741ebdc72a9390d744d8d31f2d
Yerim Choi <yerimchoi_@naver.com>
2025-07-15 19:24:48 +0900

    feat: 숙소 상세 페이지 구현

---
+++ components/RoomDetail/HeaderSection.tsx
@@ -0,0 +1,70 @@
+'use client'
+
+import { useState } from 'react'
+import { RoomType } from '@/interface'
+import { CiHeart, CiShare1 } from 'react-icons/ci'
+import { BLUR_DATA_URL } from '@/constants'
+import Image from 'next/image'
+import { AiOutlineUnorderedList } from 'react-icons/ai'
+import ImageListModal from './ImageModal'
+
+export default function HeaderSection({ data }: { data: RoomType }) {
+  const [showImageModal, setShowImageModal] = useState<boolean>(false)
+
+  return (
+    <>
+      <h1 className="text-lg mg:text-3xl font-medium px-4">{data?.title}</h1>
+      <div className="flex w-full justify-between items-center px-4">
+        <div className="underline text-xs md:text-sm mt-2">{data.address}</div>
+        <div className="flex gap-2 text-xs md:text-sm mt-2">
+          <button
+            type="button"
+            className="flex gap-2 items-center px-2 py-1.5 rounded-lg hover:bg-black/10"
+          >
+            <CiShare1 />
+            <span>공유하기</span>
+          </button>
+          <button
+            type="button"
+            className="flex gap-2 items-center px-2 py-1.5 rounded-lg hover:bg-black/10"
+          >
+            <CiHeart />
+            <span>저장</span>
+          </button>
+        </div>
+      </div>
+
+      <div className="mt-6 relative">
+        <div className="grid md:grid-cols-2 md:gap-4 gap-2 align-middle h-[400px] overflow-hidden md:rounded-lg">
+          {data?.images?.slice(0, 2)?.map((img) => {
+            return (
+              <div key={img} className=" w-full relative">
+                <Image
+                  src={img}
+                  alt="room img"
+                  style={{ objectFit: 'cover' }}
+                  placeholder="blur"
+                  blurDataURL={BLUR_DATA_URL}
+                  fill
+                />
+              </div>
+            )
+          })}
+        </div>
+        <button
+          type="button"
+          className="absolute right-6 bottom-8 bg-white px-4 py-1.5 rounded-md text-sm border-black flex gap-2 items-center hover:shadow-lg"
+          onClick={() => setShowImageModal(true)}
+        >
+          <AiOutlineUnorderedList />
+          사진 모두 보기
+        </button>
+      </div>
+      <ImageListModal
+        isOpen={showImageModal}
+        closeModal={() => setShowImageModal(false)}
+        data={data}
+      />
+    </>
+  )
+}
--- interface/index.ts
+++ interface/index.ts
@@ -24,10 +24,33 @@ export interface RoomType {
   images: string[]
   title: string
   address: string
+  desc: string
+  bedroomDesc: string
   price: number
   category: string
   lat: string
   lng: string
+  user?: UserType
+  userId?: number
+  freeCancel: boolean
+  selfCheckIn: boolean
+  officeSpace: boolean
+  hasMountainView: boolean
+  hasShampoo: boolean
+  hasFreeLaundry: boolean
+  hasAirConditioner: boolean
+  hasWifi: boolean
+  hasBarbeque: boolean
+  hasFreeParking: boolean
+}
+
+export interface UserType {
+  id: number
+  email: string
+  name?: string
+  image?: string
+  desc?: string
+  rooms: RoomType[]
 }
 
 export interface FaqType {
@@ -40,4 +63,10 @@ export interface LocationType {
   lat?: string | null
   lng?: string | null
   zoom?: number
+}
+
+export interface ParamsProps {
+  params: {
+    id: string
+  }
 }
