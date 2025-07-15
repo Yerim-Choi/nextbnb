d3b3f8714107267b2d7cb9d1b1c25bcbb53bce2a
Yerim Choi <yerimchoi_@naver.com>
2025-07-15 20:01:16 +0900

    feat: 숙박 상세 페이지 달력 컴포넌트 구현

--- components/Filter/index.tsx
+++ components/Filter/index.tsx
@@ -70,6 +70,8 @@ const CheckInFilter = () => {
       isShow={detailFilter === 'checkIn'}
     >
       <Calendar
+        next2Label={null}
+        prev2Label={null}
         className={'mt-8 mx-auto'}
         onChange={onChange}
         minDate={new Date()}
@@ -94,6 +96,8 @@ const CheckOutFilter = () => {
       isShow={detailFilter === 'checkOut'}
     >
       <Calendar
+        next2Label={null}
+        prev2Label={null}
         className={'mt-8 mx-auto'}
         onChange={onChange}
         minDate={
